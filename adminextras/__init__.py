2bfc9c93289fec7ceee142b667fc2aece732329e
Nahuel <nahuel.defosse@gmail.com>
2011-01-31 20:23:28 -0300

    Informacion de version

--- adminextras/__init__.py
+++ adminextras/__init__.py
@@ -1,3 +1,11 @@
+__doc__ = '''
+    Django admin extras is a set of tools for the django administration
+    that have become quite handy for applications development heavily
+    based on the admin interfase.
+    
+    There are third partiy resources bundled in such fam fam fam icons.
+'''
+
 
 VERSION = (0, 1, 'a')
 
