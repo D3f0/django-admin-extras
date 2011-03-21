19208bc92af89d4101c7e872768ca84b3ee721d1
Nahuel <nahuel.defosse@gmail.com>
2011-03-20 22:05:37 -0300

    Command for droping all tables from database

---
+++ adminextras/management/commands/tabledrop.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+#-*- encoding: utf-8 -*-
+# Created: 21/06/2010 by defo
+
+
+from django.core.management.base import BaseCommand, CommandError
+from django.core import management 
+from django.db import connection
+from django.conf import settings 
+import re
+from optparse import make_option
+
+class Command(BaseCommand):
+    '''
+    Reconsturcción de tablas
+    '''
+    
+    args = '<tabla tabla tabla>'
+    help = 'Elimina las tablas'
+    
+    option_list = BaseCommand.option_list + (
+        make_option('-s', '--simulate',
+            action='store_true',
+            dest='simulate',
+            default=False,
+            help='Simula la eliminacion'),
+        )
+    
+    keep = [ re.compile(exp) for exp in (r'auth_user', r'auth_session') ]
+    
+    def __init__(self, *largs, **kwargs):
+        BaseCommand.__init__(self, *largs, **kwargs)
+        from django.conf import settings
+        self.cascade = False
+        if settings.DATABASE_ENGINE:
+            self.cascade = settings.DATABASE_ENGINE.find('postgresql') >= 0
+        elif settings.DATABASES:
+            db_settings = settings.DATABASES.get('default')
+            self.cascade = db_settings.get('ENGINE').find('postgresql') >= 0
+                
+    def handle(self, *tables,  **options):
+        self.cursor = connection.cursor()
+        current_tables = connection.introspection.table_names()
+        dropable_tables = filter(lambda t: self.needs_drop(t), current_tables)
+        
+        
+        if tables:
+            if options.get('simulate'): 
+                print('\n'.join([t for t in dropable_tables if t in tables]))
+                return
+            for table in tables:
+                if table in dropable_tables:
+                    self.drop(table)
+        else:
+            # Ya tenemos las tablas, ahora vamos a ver si neceistamos SQL de Postgres
+            if options.get('simulate'): 
+                print('\n'.join(dropable_tables))
+                return
+            for table in dropable_tables:
+                self.drop(table)
+                
+        management.call_command('syncdb')
+        
+        print("OK")
+        
+    def drop(self, name):
+        '''
+        Drop a table
+        '''
+        try:
+            sql = "DROP TABLE %s %s" % (name, self.cascade and 'CASCADE' or '')
+            print("SQL:\n\t%s" % sql)
+            self.cursor.execute(sql) 
+        except Exception,e:
+            raise e
+    
+    
+    
+    def needs_drop(self, name):
+        for expr in self.keep:
+            # Es una expresion regular
+            if hasattr(expr, 'search'):
+                if expr.search(name):
+                    return False
+            # Es la misma cadena
+            elif expr == name:
+                return False
+        return True
+    
