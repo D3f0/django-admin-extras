b3379011a1ae079626c5cf2c2a9b3bb85f2263ef
Nahuel <nahuel.defosse@gmail.com>
2011-08-02 16:20:45 -0300

    Multiselect filter fix

--- adminextras/static/js/adminextras/multiselect.js
+++ adminextras/static/js/adminextras/multiselect.js
@@ -11,14 +11,17 @@ adminextras.multiselect = (function (){
 	function init(){
 		
 		$('.jqueryuimultiselect').each(function (){
+			$(this).find('option[value=""]').each(function (){
+				alert("hoa");
+			});
 			var opts = $.parseJSON($(this).attr('multiselect_attrs'));
-			console.log("Construyendo sobre", this, "con", opts);
+			console.info("Construyendo sobre", this, "con", opts);
 			var multiselect = $(this).multiselect(opts);
 			var filter_opts = $.parseJSON($(this).attr('filter_attrs'));
-			console.log("Filter", filter_opts);
+			console.info("Filter", filter_opts);
 			if (filter_opts) {
-				console.log("Convirtiendo", this, "en filtro");
-				$(this).multiselect().multiselectfilter();
+				console.debug("Convirtiendo", this, "en filtro");
+				$(this).multiselect().multiselectfilter(filter_opts);
 			}
 		});
 	}
